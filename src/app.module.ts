20ef7564944a487e05ce72557523f0b28c28a97b
unknown <kaurodrigo.joenck2@gmail.com>
2024-02-02 16:40:02 -0300

    configuração inicial do backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsuariosModule } from './usuarios/usuarios.module';
-import { SequelizeConfigModule } from './database/sequelize.module';
 import { DatabaseModule } from './database/config.module';
 
 @Module({
-  imports: [UsuariosModule, DatabaseModule, SequelizeConfigModule],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
