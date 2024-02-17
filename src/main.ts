b8e480f8960c3a1b15f481ed4ccf35f4e3475850
Kaua Rodrigo <kaurodrigo.joenck2@gmail.com>
2024-02-16 22:55:36 -0300

    teste/4

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
+    credentials: true
   });
   await app.listen(process.env.PORT, '0.0.0.0');
 }
