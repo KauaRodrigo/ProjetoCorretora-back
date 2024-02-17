24af80454ad4a8a706647757a92a94827c992b90
Kaua Rodrigo <kaurodrigo.joenck2@gmail.com>
2024-02-16 23:14:31 -0300

    teste/7

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://freitaseroepke.vercel.app',
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: '*',
     credentials: true,
-    maxAge: 86400,
   });
   await app.listen(process.env.PORT);
 }
