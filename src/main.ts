9a33463a96c43505a136555f434dbc068c72b26f
Kaua Rodrigo <kaurodrigo.joenck2@gmail.com>
2024-02-16 22:52:47 -0300

    teste/3

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'freitaseroepke.vercel.app/'
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
   });
   await app.listen(process.env.PORT, '0.0.0.0');
 }
